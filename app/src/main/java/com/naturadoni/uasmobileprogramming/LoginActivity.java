063203938ebeb87b36eeec313129597435460a8a
doniwirawan <doniwirawan166@gmail.com>
2022-07-02 14:16:04 +0800

    save in shared preferences nim and password

--- app/src/main/java/com/naturadoni/uasmobileprogramming/LoginActivity.java
+++ app/src/main/java/com/naturadoni/uasmobileprogramming/LoginActivity.java
@@ -2,7 +2,9 @@ package com.naturadoni.uasmobileprogramming;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -38,6 +40,13 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view){
                 String nim = nimLoginET.getText().toString();
                 String password = passwordLoginET.getText().toString();
+                SharedPreferences sharedPreferences;
+
+                sharedPreferences = getSharedPreferences("user_details", MODE_PRIVATE);
+                sharedPreferences.contains("nim");
+                sharedPreferences.contains("username");
+                sharedPreferences.contains("password");
+
 
                 MahasiswaModel mhs = db.getMahasiswaByNIM(nim);
 
@@ -48,8 +57,19 @@ public class LoginActivity extends AppCompatActivity {
 
                 if(mhs.getPassword().equals(password)) {
                     Toast.makeText(LoginActivity.this, "password benar", Toast.LENGTH_LONG).show();
+
+
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("nim", mhs.getNim());
+                    editor.putString("username", mhs.getFirstname() + ' ' + mhs.getLastname());
+                    editor.putString("password", mhs.getPassword());
+                    editor.apply();
+
                     Intent homeIntent = new Intent(LoginActivity.this, HomeActivity.class);
                     startActivity(homeIntent);
+
+
+
                 }
 
             }
